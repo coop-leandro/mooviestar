63442dced161986d85f88b35dd362a75b19949c5
Leandro <leandrocorneo5@unesc.net>
2024-10-29 17:45:55 -0300

    update imagem de perfil

--- user_process.php
+++ user_process.php
@@ -22,8 +22,31 @@
         $userData->email = $email;
         $userData->bio = $bio;
 
-        $userDao->update($userData);
-
+        if (isset($_FILES['image']) && !empty($_FILES['image']['tmp_name'])) {
+            $image = $_FILES['image'];
+            $imageTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+            $jpgArr = ['image/jpeg', 'image/jpg'];
+        
+            if (in_array($image['type'], $imageTypes)) {
+                if (in_array($image['type'], $jpgArr)) {
+                    $imageFile = imagecreatefromjpeg($image['tmp_name']);
+                } else {
+                    $imageFile = imagecreatefrompng($image['tmp_name']);
+                }
+        
+                if ($imageFile !== false) {
+                    $imageName = $user->imageGenerateName();
+                    imagejpeg($imageFile, './img/users/' . $imageName, 100);
+                    $userData->image = $imageName;
+                } else {
+                    $message->setMessage("Erro ao processar a imagem. Verifique se o arquivo está corrompido.", "error", "back");
+                }
+            } else {
+                $message->setMessage("Tipo inválido de imagem, insira png ou jpg!", "error", "back");
+            }
+        }
+        
+        $userDao->update($userData);  
     }else if($type === 'changepassword'){
 
     }else{
