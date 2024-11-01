4f2be2072a7bed867f6bc0aa29a9863ff0e15935
Leandro <leandrocorneo5@unesc.net>
2024-11-01 13:34:57 -0300

    movie page

--- movie.php
+++ movie.php
@@ -1,1 +1,29 @@
-<?php ?>
+<?php 
+    require_once('components/header.php');
+    require_once('models/Movie.php');
+    require_once('dao/MovieDAO.php');
+
+
+    $movieDao = new MovieDAO($conn, $BASE_URL);
+
+    $id = filter_input(INPUT_GET, 'id');
+    $movie;
+    if(empty($id)){
+        $message->setMessage('error', 'error', 'index.php');
+    } else {
+        $movie = $movieDao->findById($id);
+        if(!$movie){
+            $message->setMessage('error', 'error', 'index.php');
+        }
+    }
+
+    $UserOwnsMovie = false;
+
+    if(!empty($userData)){
+        if($userData->id === $movie->users_id){
+            $UserOwnsMovie = true;
+        }
+    }
+
+?>
+
